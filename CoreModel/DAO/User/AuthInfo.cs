ff0f91bbcd6ac5b69a251a3fdce7276942e7d1f6
TangZhihao <tjrtzh@gmail.com>
2018-03-18 11:43:01 +0800

    update user models

---
+++ CoreModel/DAO/User/AuthInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreModel.DAO.User
+{
+    public class AuthInfo
+    {
+        String BaseToken { get; set; }
+        String ExtraToken { get; set; }
+    }
+}
---
+++ CoreModel/DAO/User/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreModel.DAO.User
+{
+    public class User
+    {
+        String Id { get; set; }
+        String Email { get; set; }
+        String Passcode { get; set; }
+    }
+}
